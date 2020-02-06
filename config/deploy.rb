12f7e69c0f4ff993ab7313c2dc922698d2aae0df
Bhavesh Prajapat <BPrajapat1@sheffield.ac.uk>
2020-02-06 22:04:48 +0000

    Added thin to Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,3 +48,5 @@ set :linked_dirs, fetch(:linked_dirs, [ ]).push('log', 'tmp/pids', 'tmp/cache', 
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
+after 'deploy:publishing', 'thin:restart'
+
